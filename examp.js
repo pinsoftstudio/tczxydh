6eff8b3ea0b5e5543eb8f244abd42ed025c65b65
Program <ProgramCX@Outlook.com>
2024-08-07 19:53:09 +0800

    “学校风景”页面改为播放校歌

--- examp.js
+++ examp.js
@@ -6,11 +6,17 @@ try{
 }
 let ph,played=false,playm=pld;
 if(localStorage.getItem('playm')=='true'){
+    //如果是默认播放
     playm=true;
     $('#music>.btn.play').remove();
     $('#music>.tip').text('我们时刻准备趁浏览器不备播放音乐');
 }else if(localStorage.getItem('playm')=='false'){
-    document.getElementById('audio').pause();
+    //如果不是默认播放，就暂停播放
+    if(playss){
+        document.getElementById('ssaudio').pause();
+    }else{
+        document.getElementById('audio').pause();
+    }
     playm=false;
     $('#music>.btn.noplay').remove();
     $('#music>.tip').text('我们不会播放音乐');
@@ -27,16 +33,39 @@ function init() {
 }
 function start() {
     let pics=document.querySelectorAll('#content>.pic');
+    //当用户滑动页面
     document.addEventListener('scroll',()=>{
+        //如果需要播放但是还未播放
         if(playm && (!played)){
-            if(document.getElementById('audio').played.length>0){
-                played=true;
-                $('#music>.btn').remove();
-                $('#musctrl>.btn').text('暂停');
-                $('#musctrl>.btn').removeClass('play');
-                $('#music>.tip').text('我们已开始播放音乐');
-            }else
-            document.getElementById('audio').play();
+            //如果需要播放校歌
+            if(playss){
+                //如果已经开始播放
+                if(document.getElementById('ssaudio').played.length>0){
+                    played=true;
+                    $('#music>.btn').remove();
+                    $('#musctrl>.btn').text('暂停');
+                    $('#musctrl>.btn').removeClass('play');
+                    $('#music>.tip').text('我们已开始播放音乐');
+                }else{
+                    //没有开始播放就开始播放
+                    document.getElementById('ssaudio').play();
+                }
+                
+            }else{
+                //如果要播放《运动员进行曲》
+                if(document.getElementById('audio').played.length>0){
+                    //如果已经开始播放
+                    played=true;    //改变状态为已经播放
+                    $('#music>.btn').remove();
+                    $('#musctrl>.btn').text('暂停');
+                    $('#musctrl>.btn').removeClass('play');
+                    $('#music>.tip').text('我们已开始播放音乐');
+                }else{
+                    //没有开始播放就开始播放
+                    document.getElementById('audio').play();
+                }
+                
+            }
         }
         let t=$('html').scrollTop();
         pics.forEach(pic => {
